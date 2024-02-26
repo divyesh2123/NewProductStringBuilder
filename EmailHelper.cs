1f1f15223bd159092dd2dbd03a7de1791cbbc5cb
lenovo <lenovo@LAPTOP-BLNTEBH7>
2024-02-26 10:20:02 +0530

    Add project files.

---
+++ EmailHelper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewProductStringBuilder
+{
+   static public class EmailHelper
+    {
+        static public void sendEmail(string to, string body, string title)
+        {
+            using (MailMessage mail = new MailMessage())
+            {
+                mail.From = new MailAddress("divyesh2198@gmail.com");
+                mail.To.Add(to);
+                mail.Subject = title;
+                mail.Body = body;
+                mail.IsBodyHtml = true;
+                //mail.Attachments.Add(new Attachment("D:\\TestFile.txt"));//--Uncomment this to send any attachment  
+                using (SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587))
+                {
+                    smtp.Credentials = new NetworkCredential("divyesh2198@gmail.com", "");
+                    smtp.EnableSsl = true;
+                    smtp.Send(mail);
+                }
+            }
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewProductStringBuilder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (EDMSEntities d= new EDMSEntities())
+            {
+                var users = d.Users.ToList();
+                var mytemplate = d.EmailTemplates.FirstOrDefault(y => y.NameOf == "ProductEmail");
+              
+                foreach(var d1 in users)
+                {
+                    var p = new StringBuilder(mytemplate.Body);
+
+                    p.Replace("##FirstName##", d1.Name);
+                    p.Replace("##LastName##", d1.UserName);
+                    p.Replace("##ProductName##", "New Course for nest.js");
+                    EmailHelper.sendEmail(d1.Email,p.ToString(),mytemplate.Title);
+
+                }
+            }
+        }
+    }
+}
